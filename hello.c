8565e051f0044c5e9edd3e590c0e71c0d78db351
Jan-de-Wit <Jan-de-Wit@users.noreply.github.com>
2022-08-10 08:30:01 +0000

    automated commit by submit50

--- hello.c
+++ hello.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(void) {
+int main(void)
+{
+    //Gets user input
     string answer = get_string("What is your name? ");
+
+    //Prints "Hello <answer>"
     printf("Hello %s!\n", answer);
 }
