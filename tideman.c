6abdab1e61f0f4a25c22451732a8ac43064d8ea4
Jan-de-Wit <Jan-de-Wit@users.noreply.github.com>
2022-08-16 13:55:44 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -217,22 +217,21 @@ void print_winner(void)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        bool lost = false;
+        bool isLoser = false;
         for (int j = 0; j < candidate_count; j++)
         {
             if (locked[j][i])
             {
-                lost = true;
+                isLoser = true;
                 break;
             }
 
         }
 
-        if (lost) continue;
-        if(lost == false)
+        if (isLoser) continue;
+        if(!isLoser)
         {
-            printf("%s\n", candidates[pairs[i].winner]);
-            return;
+            printf("%s\n", candidates[i]);
         }
     }
     return;
