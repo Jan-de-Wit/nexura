dd23f209230e14f9116b716cbb577ac3433e16f5
Jan-de-Wit <Jan-de-Wit@users.noreply.github.com>
2022-08-17 11:12:01 +0000

    automated commit by check50 [check50=True]

--- volume.c
+++ volume.c
@@ -35,42 +35,38 @@ int main(int argc, char *argv[])
 
     // TODO: Copy header from input file to output file
     //fread to read 44 bytes and fwrite to write those 44 bytes to output.wav
-    uint8_t *header = malloc(HEADER_SIZE);
-    fread(header, HEADER_SIZE, 1, input);
-    fwrite(&input, HEADER_SIZE, 1, output);
-    free(header);
-
-    printf("Header got copied\n");
+    uint8_t header[44];
+    fread(&header, HEADER_SIZE, 1, input);
+    fwrite(&header, HEADER_SIZE, 1, output);
 
     // TODO: Read samples from input file and write updated data to output file
     int freadError = 0;
     int n = 0;
-    while (freadError == 0)
-    {
-        printf("While loop initiated\n");
-        int16_t sample = 0;
-        int16_t *p = &sample;
+    int16_t sample = 0;
 
-        printf("Variables initialized\n");
-        if (fread(p, sizeof(int16_t), 1, input) == 1)
-        {
-            printf("If statement worked");
-            sample = sample * factor;
-            fwrite(p, sizeof(int16_t), 1, output);
-            n++;
-            printf("Succesfull write %i times\n", n);
-        }
-        else
+    do
+    {
+        if (fread(&sample, sizeof(int16_t), 1, input) < 1)
         {
             freadError = 1;
         }
+        else
+        {
+            sample = sample * factor;
+            fwrite(&sample, sizeof(int16_t), 1, output);
+            n++;
+        }
     }
-    // length of samples
+    while (freadError == 0);
 
-    // for each sample (int16_t):
-        //Read sample from input.wav
-        //multiply sample by factor
-        //write the new sample to output.wav
+    if (n == 352.800 / 2)
+    {
+        printf("$$$ looped %i times\n", n);
+    }
+    else
+    {
+        printf("###looped %i times\n", n);
+    }
 
     // Close files
     fclose(input);
