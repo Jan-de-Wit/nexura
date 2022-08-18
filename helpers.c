3a54a636cd423c3fdfe25ab8f6b417ac68695461
Jan-de-Wit <Jan-de-Wit@users.noreply.github.com>
2022-08-18 16:45:52 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -23,26 +23,33 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     RGBTRIPLE temp;
-    int n;
+    int n = width / 2;
 
     if (width % 2 == 0)
     {
-        n = width / 2;
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j <= n; j++)
+            {
+                temp = image[i][j];
+                image[i][j] = image[i][width - 1 - j];
+                image[i][width - 1 - j] = temp;
+            }
+        }
     }
     else
     {
-        n = width / 2 - 1;
-    }
-    
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j <= n; j++)
+        for (int i = 0; i < height; i++)
         {
-            temp = image[i][j];
-            image[i][j] = image[i][width - 1 - j];
-            image[i][width - 1 - j] = temp;
+            for (int j = 0; j < n; j++)
+            {
+                temp = image[i][j];
+                image[i][j] = image[i][width - j];
+                image[i][width - j] = temp;
+            }
         }
     }
+
     return;
 }
 
