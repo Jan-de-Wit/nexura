6c903d8c7b21cb6419510243abe87847cd90e276
Jan-de-Wit <Jan-de-Wit@users.noreply.github.com>
2022-08-19 10:27:43 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -78,7 +78,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                     }
                 }
             }
-            int red = round(allRed / (float)counter);
+            int red = round(allRed / counter);
             int green = round(allGreen / counter);
             int blue = round(allBlue / counter);
 
