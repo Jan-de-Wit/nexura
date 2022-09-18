9fc111a9d39bab463daa8dea31e7e5716aa93cfc
Jan-de-Wit <Jan-de-Wit@users.noreply.github.com>
2022-09-18 17:55:47 +0000

    automated commit by check50 [check50=True]

--- 3.sql
+++ 3.sql
@@ -1,2 +1,2 @@
 -- Outputs the names of the top 5 longest songs in descending order of length
-SELECT name FROM songs ORDER BY duration_ms LIMIT 5;
+SELECT name FROM songs ORDER BY duration_ms DESC LIMIT 5;
